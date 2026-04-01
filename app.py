9d5a4f02710e8f874263d4747f6be9259bf8f679
苏勇大哥 <suyongdage@suyongdagedeMacBook-Air.local>
2026-03-31 21:12:39 -0700

    fix port binding

--- app.py
+++ app.py
@@ -310,5 +310,7 @@ def index():
     )
 
 
+
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
